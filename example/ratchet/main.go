8a464ecba064d29d394c7c3b35e71592fce65410
othon <othonhugo@users.noreply.github.com>
2024-05-23 22:15:37 -0300

    refactor: create 'setup.go' file in 'example/ratchet' directory

--- example/ratchet/main.go
+++ example/ratchet/main.go
@@ -1,18 +1,13 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/othonhugo/doubleratchet"
-	"github.com/othonhugo/doubleratchet/crypto/ecdh"
-)
-
-var alice, bob = setup()
+var Message = []byte("hello, there!")
 
 func main() {
-	message := []byte("hello, there!")
+	alice, bob := Setup()
 
-	ciphered, err := alice.Send(message, nil)
+	ciphered, err := alice.Send(Message, nil)
 
 	if err != nil {
 		panic(err)
@@ -26,23 +21,4 @@ func main() {
 
 	fmt.Printf("Ciphertext: %2X\n", ciphered.Ciphertext)
 	fmt.Printf("Plaintext: %s\n", unciphered.Plaintext)
-}
-
-func setup() (doubleratchet.DoubleRatchet, doubleratchet.DoubleRatchet) {
-	alicePri, _ := ecdh.GeneratePrivateKey()
-	bobPri, _ := ecdh.GeneratePrivateKey()
-
-	alice, err := doubleratchet.New(alicePri.Bytes(), bobPri.PublicKey().Bytes())
-
-	if err != nil {
-		panic(err)
-	}
-
-	bob, err := doubleratchet.New(bobPri.Bytes(), alicePri.PublicKey().Bytes())
-
-	if err != nil {
-		panic(err)
-	}
-
-	return alice, bob
 }
